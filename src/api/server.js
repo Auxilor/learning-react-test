5a88cbb8e24f0a4c12e25f59cf7e42dd1f73579d
Auxilor <william.favierparsons1@gmail.com>
2021-05-20 14:14:30 +0100

    Changed server again

--- src/api/server.js
+++ src/api/server.js
@@ -7,10 +7,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.use(express.json())
   .use(express.urlencoded({ extended: true }));
 
