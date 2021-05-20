adbfeb12d448cb7e023051e614b2eb6a76eee568
Auxilor <william.favierparsons1@gmail.com>
2021-05-20 13:54:21 +0100

    More heroku shenanigans

--- src/api/server.js
+++ src/api/server.js
@@ -5,7 +5,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(express.json())
   .use(express.urlencoded({ extended: true }));
