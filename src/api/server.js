79ab787062472383080d2b1ddc725881de459f5d
Auxilor <william.favierparsons1@gmail.com>
2021-05-20 13:47:20 +0100

    More heroku tweaking

--- src/api/server.js
+++ src/api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(express.json())
   .use(express.urlencoded({ extended: true }));
